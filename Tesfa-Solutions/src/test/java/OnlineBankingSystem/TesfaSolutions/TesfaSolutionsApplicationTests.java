48a94a644fa5c5ae0e03bda6102001c74e5ef030
YoditAbraha <107884901+YoditAbraha@users.noreply.github.com>
2023-09-11 22:42:59 -0500

    The project has been added to the kabbee repository:)

---
+++ Tesfa-Solutions/src/test/java/OnlineBankingSystem/TesfaSolutions/TesfaSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package OnlineBankingSystem.TesfaSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesfaSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
