ec7caff3831feb8738f74c7046cdfe5dc6cc64f4
Mussie <mussieayub@gmail.com>
2023-09-16 14:31:35 -0500

    find all

--- Tesfa-Solutions/src/main/java/OnlineBankingSystem/TesfaSolutions/service/impl/UserServiceImpl.java
+++ Tesfa-Solutions/src/main/java/OnlineBankingSystem/TesfaSolutions/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import OnlineBankingSystem.TesfaSolutions.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,7 +23,25 @@ public class UserServiceImpl implements UserService {
     }
 
     public Optional<User> findByUserName(String username) {
+
         return repository.findByUsername(username);
     }
+
+//    @Override
+//    public User getUserByID(int id) {
+//        Optional<User> user=repository.findById(id);
+//        if(user.isPresent()){
+//            return user.get();
+//        }else{
+//            throw  new RuntimeException("user doesn't exist");
+//        }
+//
+//    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return repository.findAll();
+    }
+
 
 }
